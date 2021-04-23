f176b2f29f81c40a57ccc5f2334fae8a2579d99a
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 07:41:27 -0400

    header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="page-header">
+      <h1 className="Title">
+        Google Books Search with React <br></br></h1>
+        <h3>Search and Save books <br></br>
+            you've been waiting to read!</h3>
+    </div>
+  );
+}
+export default Header;
