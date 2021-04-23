b99d7cb8d57b86eb272bcaa6dbee98b17ebc3808
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 08:22:44 -0400

    Search bar

---
+++ client/src/components/SearchBarResullts/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function SearchBar() {
+  return (
+    <div>
+      <h3>Let's Search for a Book</h3>
+      <div className="container searchBar"></div>
+    </div>
+  );
+}
+export default SearchBar;
