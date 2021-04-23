4d41d64bad49350942f70d11f49d36e44ebd2aca
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 07:32:46 -0400

    Create Jumbotron.js

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div id="community" className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">
+          <p>
+          Google Book Seach <br/>
+            Let's Get Looking!
+          </p>
+        </h1>
+      </div>
+    </div>
+  );
+}
+export default Jumbotron;
