a464bf3a64a6f7dac651961e6c8ab57b4a5e6441
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 16:57:53 -0400

    Create Book.js

---
+++ models/Book.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  authors: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  synopsis: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
