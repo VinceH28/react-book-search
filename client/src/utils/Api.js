0cfccb34eec7b8e4f55a905f249e3d136f32fde3
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-23 07:24:36 -0400

    Create Api.js

---
+++ client/src/utils/Api.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="page-header">
+      <h1 className="Title">
+        Google Books with React <br></br></h1>
+        <h4>Search for and Save the books <br></br>
+            you've been eager to read!</h4>
+    </div>
+  );
+}
+export default Header;
